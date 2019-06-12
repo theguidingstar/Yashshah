e8af1cba07ee1e347c3225fef475a191bbb3be3e
Yash Shah <yashshh123@gmail.com>
2019-06-12 15:24:09 +0530

    Relocation of the components.
    Relocation of the components from the folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './common/intro/intro.component';
-import { MainFrameComponent } from './commmon/main-frame/main-frame.component';
+import { MainFrameComponent } from './common/main-frame/main-frame.component';
 
 @NgModule({
   declarations: [
